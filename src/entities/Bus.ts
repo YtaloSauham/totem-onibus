f1dadcd8305a96c9b9e431e3c29f71ce9ed8b883
YtaloSauham <ytalo.sauhan@hotmail.com>
2022-12-27 01:25:11 -0300

    initial refactoring backend

---
+++ src/entities/Bus.ts
@@ -0,0 +1,24 @@
+import pgp from 'pg-promise'
+
+export default class Bus {
+    
+    constructor(){
+
+    }
+
+
+    async getByBairroAndLocation(Input: InputProps ){
+    const connection= pgp()(`postgres://postgres:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_USER}:${process.env.DATABASE_PORT}/${process.env.DATABASE_DATA}`)
+    const consultResult = await connection.query(" select item_de_itinerario.nome, tipo_item_itinerario.descricao, terminal_linhas.plataforma,linhas_de_onibus.codigo,linhas_de_onibus.nome,linhas_de_onibus.caracteristica,itinerario.ordem from itinerario inner join linhas_de_onibus on  itinerario.fk_idlinhas= linhas_de_onibus.idlinhas inner join item_de_itinerario on itinerario.fk_iditem = item_de_itinerario.iditem inner join tipo_item_itinerario on item_de_itinerario.fk_idtipo= tipo_item_itinerario.idtipo inner join terminal_linhas on terminal_linhas.fk_idlinhas = linhas_de_onibus.idlinhas where tipo_item_itinerario.descricao = $1 and terminal_linhas.fk_idterminal = $2 and item_de_itinerario.nome = $3",[Input.sentido,Input.terminalId,Input.local]);
+    await connection.$pool.end();
+    return consultResult
+    }
+
+
+}
+
+interface InputProps{
+    sentido:string,
+    terminalId:string,
+    local:string
+}
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import pgp from 'pg-promise';
+import Bus from './entities/bus';
 require('dotenv')
 /*
 
@@ -27,16 +28,13 @@ const client = new Client({
 app.use(express.json());
 
 app.get(`/terminal/:id/sentido`, async function (request, response){
-
+    const getBus = new Bus();
     const terminalId = request.params.id;
     const sentido = request.body.sentido;
-    const localParams = request.body.local;
-    console.log(sentido,localParams);
-    const connection= pgp()(`postgres://postgres:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_USER}:${process.env.DATABASE_PORT}/${process.env.DATABASE_DATA}`)
-    const terminal = await connection.query(" select item_de_itinerario.nome, tipo_item_itinerario.descricao, terminal_linhas.plataforma,linhas_de_onibus.codigo,linhas_de_onibus.nome,linhas_de_onibus.caracteristica,itinerario.ordem from itinerario inner join linhas_de_onibus on  itinerario.fk_idlinhas= linhas_de_onibus.idlinhas inner join item_de_itinerario on itinerario.fk_iditem = item_de_itinerario.iditem inner join tipo_item_itinerario on item_de_itinerario.fk_idtipo= tipo_item_itinerario.idtipo inner join terminal_linhas on terminal_linhas.fk_idlinhas = linhas_de_onibus.idlinhas where tipo_item_itinerario.descricao = $1 and terminal_linhas.fk_idterminal = $2 and item_de_itinerario.nome = $3",[sentido,terminalId,localParams]);
-    await connection.$pool.end();
-    console.log(terminal)
-    response.json(terminal);
+    const local = request.body.local;
+    
+    const busResult = getBus.getByBairroAndLocation({terminalId:request.params.id,sentido:request.body.sentido,local:request.body.local})
+    response.json(busResult);
   
 
 })

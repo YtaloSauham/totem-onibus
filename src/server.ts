77d75b08dc343abe91e11698faf27f19b36c3ee0
YtaloSauham <ytalo.sauhan@hotmail.com>
2022-12-27 08:26:42 -0300

    refactiorin stand

--- src/server.ts
+++ src/server.ts
@@ -28,13 +28,16 @@ const client = new Client({
 app.use(express.json());
 
 app.get(`/terminal/:id/sentido`, async function (request, response){
-    const getBus = new Bus();
+   
     const terminalId = request.params.id;
     const sentido = request.body.sentido;
     const local = request.body.local;
+
+    const connection= pgp()(`postgres://postgres:postgres://postgres:3124@localhost:5432/postgres`)
+    const consultBairroResult = await connection.query(" select item_de_itinerario.nome, tipo_item_itinerario.descricao, terminal_linhas.plataforma,linhas_de_onibus.codigo,linhas_de_onibus.nome,linhas_de_onibus.caracteristica,itinerario.ordem from itinerario inner join linhas_de_onibus on  itinerario.fk_idlinhas= linhas_de_onibus.idlinhas inner join item_de_itinerario on itinerario.fk_iditem = item_de_itinerario.iditem inner join tipo_item_itinerario on item_de_itinerario.fk_idtipo= tipo_item_itinerario.idtipo inner join terminal_linhas on terminal_linhas.fk_idlinhas = linhas_de_onibus.idlinhas where tipo_item_itinerario.descricao = $1 and terminal_linhas.fk_idterminal = $2 and item_de_itinerario.nome = $3",[sentido,terminalId,local]);
+    await connection.$pool.end();
     
-    const busResult = getBus.getByBairroAndLocation({terminalId:request.params.id,sentido:request.body.sentido,local:request.body.local})
-    response.json(busResult);
+    response.json(consultBairroResult);
   
 
 })
@@ -44,11 +47,11 @@ app.get(`/terminal/:id/linha`, async function (request, response){
     const linhaRequest= request.body.linha;
     const terminalId= request.params.id;
     console.log(linhaRequest);
-    const connection= pgp()(`postgres://postgres:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_USER}:${process.env.DATABASE_PORT}/${process.env.DATABASE_DATA}`)
-    const terminal = await connection.query(" select item_de_itinerario.nome, tipo_item_itinerario.descricao, terminal_linhas.plataforma,linhas_de_onibus.codigo,linhas_de_onibus.nome,linhas_de_onibus.caracteristica,itinerario.ordem from itinerario inner join linhas_de_onibus on  itinerario.fk_idlinhas= linhas_de_onibus.idlinhas inner join item_de_itinerario on itinerario.fk_iditem = item_de_itinerario.iditem inner join tipo_item_itinerario on item_de_itinerario.fk_idtipo= tipo_item_itinerario.idtipo inner join terminal_linhas on terminal_linhas.fk_idlinhas = linhas_de_onibus.idlinhas where linhas_de_onibus.codigo =  $1 and terminal_linhas.fk_idterminal = $2",[linhaRequest,terminalId]);
+    const connection= pgp()(`postgres://postgres:postgres://postgres:3124@localhost:5432/postgres`)
+    const consultLinhaResult = await connection.query(" select item_de_itinerario.nome, tipo_item_itinerario.descricao, terminal_linhas.plataforma,linhas_de_onibus.codigo,linhas_de_onibus.nome,linhas_de_onibus.caracteristica,itinerario.ordem from itinerario inner join linhas_de_onibus on  itinerario.fk_idlinhas= linhas_de_onibus.idlinhas inner join item_de_itinerario on itinerario.fk_iditem = item_de_itinerario.iditem inner join tipo_item_itinerario on item_de_itinerario.fk_idtipo= tipo_item_itinerario.idtipo inner join terminal_linhas on terminal_linhas.fk_idlinhas = linhas_de_onibus.idlinhas where linhas_de_onibus.codigo =  $1 and terminal_linhas.fk_idterminal = $2",[linhaRequest,terminalId]);
     await connection.$pool.end();
-    console.log(terminal)
-    response.json(terminal);
+    console.log(consultLinhaResult)
+    response.json(consultLinhaResult);
   
 
 })

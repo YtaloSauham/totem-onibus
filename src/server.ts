95149203c0eeac1d443d7b1990ba4994141067a8
YtaloSauham <ytalo.sauhan@hotmail.com>
2022-12-28 10:27:44 -0300

    codigo clean

--- src/server.ts
+++ src/server.ts
@@ -5,17 +5,6 @@ require('dotenv').config()
 
 
 
-/*
-
-const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: '3124',
-    port:5432,
-})
-*/
-
 
 const app = express();
 
@@ -26,37 +15,29 @@ app.use((_req, res, next) => {
   next();
 });
 
-/*
-const pool = new Pool({
-    connectionString,
-  })
 
-const client = new Client({
-    connectionString,
-  })
-*/
 app.use(express.json());
 
-app.get(`/terminal/:id/sentido/:sentido/:local`, async function (request, response){
-   
-    const terminalId = request.params.id;
-    const sentido = request.params.sentido;
-    const local = request.params.local;
-    const bus = new Bus();
-    const busResult = await bus.getByBairroAndLocation({terminalId,local,sentido})
-    response.json(busResult);
-  
+app.get(`/terminal/:id/sentido/:sentido/:local`, async function (request, response) {
+
+  const terminalId = request.params.id;
+  const sentido = request.params.sentido;
+  const local = request.params.local;
+  const bus = new Bus();
+  const busResult = await bus.getByBairroAndLocation({ terminalId, local, sentido })
+  response.json(busResult);
+
 
 })
 
-app.get(`/terminal/:id/:linha`, async function (request, response){
-    const linha= request.params.linha;
-    const terminalId= request.params.id;
-    const bus = new Bus();
-    const busResult = await bus.getLinha({linha,terminalId})
-    response.json(busResult);
-    
-  
+app.get(`/terminal/:id/:linha`, async function (request, response) {
+  const linha = request.params.linha;
+  const terminalId = request.params.id;
+  const bus = new Bus();
+  const busResult = await bus.getLinha({ linha, terminalId })
+  response.json(busResult);
+
+
 
 })
 app.listen(4000)
--- src/test/GetBairro.test.ts
+++ src/test/GetBairro.test.ts
@@ -1,32 +1,31 @@
 import axios from "axios";
 
 
-test("Deve trazer os onibus de um determinado terminal,sentido e localização", async function (){
-   
+test("Deve trazer os onibus de um determinado terminal,sentido e localização", async function () {
+
     const terminalTest = 44;
 
     const resultCorrect = [
         {
-          nome: 'UEMA-IPASE',
-          descricao: 'BAIRRO',
-          plataforma: 'A2',
-          codigo: 'T070',
-          caracteristica: 'Amarelo',
-          ordem: 2
+            nome: 'UEMA-IPASE',
+            descricao: 'BAIRRO',
+            plataforma: 'A2',
+            codigo: 'T070',
+            caracteristica: 'Amarelo',
+            ordem: 2
         }
-      ]
-  
+    ]
 
-    const body = JSON.stringify({
-        sentido:"BAIRRO",
-        local: "Cohab"
-    })
+
+
+    const sentido = "BAIRRO"
+    const local = "Cohab"
+
 
     axios({
-        url:`http://localhost:4000/terminal/${terminalTest}/sentido`,
-        method:"get",
-        data:body
-    }).then(data =>{
+        url: `http://localhost:4000/terminal/${terminalTest}/sentido/${sentido}/${local}`,
+        method: "get"
+    }).then(data => {
         expect(data.data).toEqual(resultCorrect)
     })
 })
